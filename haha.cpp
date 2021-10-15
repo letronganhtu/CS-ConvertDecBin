d864cf0f9c581a9f679e2848074b42c95c4c868b
Le Trong Anh Tu <letronganhtu@gmail.com>
2021-10-15 13:17:57 +0700

    perfect

--- haha.cpp
+++ haha.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
 using namespace std;
 
+class Cat {
+public:
+  string toString() {
+    
+  }
+};
+
 int main() {
-  cout << "Hello world";
+  Cat kitty;
+  cout << kitty.toString();
   return 0;
 }
