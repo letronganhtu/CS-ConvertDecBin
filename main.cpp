c90d1ee34fab73fea3c73c41956f010fe768fda7
Le Trong Anh Tu <letronganhtu@gmail.com>
2021-11-02 21:47:27 +0700

    To Integer completed

--- main.cpp
+++ main.cpp
@@ -1,32 +1,36 @@
 ﻿#include <iostream>
 #include <string>
 #include <regex>
+#include <cmath>
 using namespace std;
 
-/// <summary>
-/// Convert from Binary to Integer 8 bit in Decimal (include Unsigned and Signed)
-/// </summary>
-class BinaryTo8BitInteger {
+// Convert from Binary to Integer 8/16/32 bits in Decimal (include Unsigned and Signed)
+class BinaryToInteger {
 private:
-    string _binarySequence;
+    int _numberOfBit; // Number of bits in string
+    string _binarySequence; // Represent bit sequences in a string
 
 public:
-    BinaryTo8BitInteger(string binary) {
+    // Require user must initialize 2 value as parameter above in constructor
+    BinaryToInteger(int number, string binary) {
+        _numberOfBit = number;
         _binarySequence = binary;
-        if (_binarySequence.length() > 8) {
-            _binarySequence = _binarySequence.substr(_binarySequence.length() - 8, 8);
+
+        if (_binarySequence.length() > _numberOfBit) { // The length of a string is longer than number of bits
+            // Take the sub sequence from right to left until its length equal to number of bits
+            _binarySequence = _binarySequence.substr(_binarySequence.length() - _numberOfBit, _numberOfBit);
         }
-        else if (_binarySequence.length() < 8) {
-            while (_binarySequence.length() != 8) {
+        else if (_binarySequence.length() < _numberOfBit) { // The length of a string is shorter than number of bits
+            while (_binarySequence.length() != _numberOfBit) {
+                // Add '0' into the first index of the sequence until its length equal to number of bits
                 _binarySequence = "0" + _binarySequence;
             }
         }
     }
 
+// Edit tomorrow
 public:
-    /// <summary>
-    /// Check if the input string is a binary sequence or not
-    /// </summary>
+    // Check if the input string is a binary sequence or not
     bool isValid() {
         const string pattern = "\\b[0,1]*\\b";
         regex validBinary(pattern);
@@ -36,7 +40,28 @@ public:
     }
 
     int convertToSignedInteger() {
-        
+        int res = 0;
+
+        for (int i = 0; i < _binarySequence.length(); i++) {
+            if (_binarySequence[i] == '1') {
+                if (i == 0) res = -pow(2, _binarySequence.length() - 1);
+                else res += pow(2, _binarySequence.length() - 1 - i);
+            }
+        }
+
+        return res;
+    }
+
+    unsigned int convertToUnsignedInteger() {
+        unsigned int res = 0;
+
+        for (int i = 0; i < _binarySequence.length(); i++) {
+            if (_binarySequence[i] == '1') {
+                res += pow(2, _binarySequence.length() - 1 - i);
+            }
+        }
+
+        return res;
     }
 };
 
@@ -44,5 +69,7 @@ int main() {
     string binarySequence;
     cin >> binarySequence;
     BinaryTo8BitInteger p(binarySequence);
+    cout << p.convertToSignedInteger() << endl;
+    cout << p.convertToUnsignedInteger();
     return 0;
 }
