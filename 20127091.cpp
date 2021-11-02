fe54b57ae81fa728f6b8d368a2f86ff4f862031d
Le Trong Anh Tu <88422176+letronganhtu@users.noreply.github.com>
2021-11-02 20:55:05 +0700

    Add files via upload

---
+++ 20127091.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <string>
+#include <regex>
+using namespace std;
+
+/// <summary>
+/// Convert from Binary to Integer 8 bit in Decimal (include Unsigned and Signed)
+/// </summary>
+class BinaryTo8BitInteger {
+private:
+    string _binarySequence;
+
+public:
+    BinaryTo8BitInteger(string binary) {
+        _binarySequence = binary;
+        if (_binarySequence.length() > 8) {
+            _binarySequence = _binarySequence.substr(_binarySequence.length() - 8, 8);
+        }
+        else if (_binarySequence.length() < 8) {
+            while (_binarySequence.length() != 8) {
+                _binarySequence = "0" + _binarySequence;
+            }
+        }
+    }
+
+public:
+    /// <summary>
+    /// Check if the input string is a binary sequence or not
+    /// </summary>
+    bool isValid() {
+        const string pattern = "\\b[0,1]*\\b";
+        regex validBinary(pattern);
+        
+        bool result = regex_match(_binarySequence, validBinary);
+        return result;
+    }
+
+    int convertToSignedInteger() {
+        
+    }
+};
+
+int main() {
+    string binarySequence;
+    cin >> binarySequence;
+    BinaryTo8BitInteger p(binarySequence);
+    return 0;
+}
